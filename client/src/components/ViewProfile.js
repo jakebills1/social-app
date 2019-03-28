f2d17ce620f94c5be2e767ab014f16594cc46121
Jake Bills <jakebills1@gmail.com>
2019-03-28 17:24:44 -0600

    add posts to profile

--- client/src/components/ViewProfile.js
+++ client/src/components/ViewProfile.js
@@ -3,12 +3,16 @@ import axios from 'axios';
 import { withRouter, } from 'react-router-dom';
 
 class ViewProfile extends React.Component {
-  state = { profile: {}, }
+  state = { profile: {}, posts: [] }
 
   componentDidMount() {
     axios.get(`/api/friends/${this.props.match.params.id}`)
       .then( res => {
         this.setState({ profile: res.data})
+      })
+    axios.get(`/api/friends/${this.props.match.params.id}/posts`)
+      .then( res => {
+        this.setState({posts: res.data, })
       })
   }
   render() {
